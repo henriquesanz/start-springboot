d6dbc94e616ec2b898d5fa06674da32c0c708163
Henrique Paiva <josehenriquedsp@gmail.com>
2021-07-21 10:22:32 -0300

    Service layer, component registration

--- src/main/java/com/henriquepaiva/course/repositories/UserRepository.java
+++ src/main/java/com/henriquepaiva/course/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package com.henriquepaiva.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.henriquepaiva.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-	
-	
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{	
 }
