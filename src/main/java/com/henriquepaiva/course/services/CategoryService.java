54c0205238b27a834059808f82ce3216fc776bc7
Henrique Paiva <josehenriquedsp@gmail.com>
2021-07-21 15:15:51 -0300

    Create entity category

---
+++ src/main/java/com/henriquepaiva/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.henriquepaiva.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.henriquepaiva.course.entities.Category;
+import com.henriquepaiva.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get();
+	}
+
+}
