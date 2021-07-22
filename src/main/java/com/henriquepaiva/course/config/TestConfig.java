c8e67b0883b53afb7fbfabb9823147ed90e6bf67
Henrique Paiva <josehenriquedsp@gmail.com>
2021-07-21 21:30:36 -0300

    Payment, one-to-one association

--- src/main/java/com/henriquepaiva/course/config/TestConfig.java
+++ src/main/java/com/henriquepaiva/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.henriquepaiva.course.entities.Category;
 import com.henriquepaiva.course.entities.Order;
 import com.henriquepaiva.course.entities.OrderItem;
+import com.henriquepaiva.course.entities.Payment;
 import com.henriquepaiva.course.entities.Product;
 import com.henriquepaiva.course.entities.User;
 import com.henriquepaiva.course.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
+		
 		
 	}
 	
